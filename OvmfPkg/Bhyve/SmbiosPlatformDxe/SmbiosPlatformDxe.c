19bf14624f2cfe6d5998c30679aa65bb66fa7e8f
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-06-11 15:58:37 -0400

    Update SmbiosPlatformDxe.c

--- OvmfPkg/Bhyve/SmbiosPlatformDxe/SmbiosPlatformDxe.c
+++ OvmfPkg/Bhyve/SmbiosPlatformDxe/SmbiosPlatformDxe.c
@@ -12,9 +12,9 @@
 #include "SmbiosPlatformDxe.h"
 
 #define TYPE0_STRINGS \
-  "EFI Development Kit II / OVMF\0"     /* Vendor */ \
-  "0.0.0\0"                             /* BiosVersion */ \
-  "02/06/2015\0"                        /* BiosReleaseDate */
+  "CECL\0"     /* Vendor */ \
+  "1960\0"                             /* BiosVersion */ \
+  "06/19/1960\0"                        /* BiosReleaseDate */
 //
 // Type definition and contents of the default Type 0 SMBIOS table.
 //
